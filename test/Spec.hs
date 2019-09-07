390c0fdef9d0b8a5a644e01ee64276ae6ed4aef6
fouche <tetrapharmakon@gmail.com>
2019-09-07 22:35:45 +0200

    made tests work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
 module Spec where
 
-import Lib
+import Multinomiala
 import Test.Hspec
 import Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
 import Test.QuickCheck
@@ -10,10 +10,21 @@ instance Arbitrary Mono where
     c <- arbitrary
     Mono c <$> arbitrary
 
+associative :: Eq a => (a -> a -> a) -> a -> a -> a -> Bool
+associative f x y z = f (f x y) z == f x (f y z)
+
+commutative :: Eq a => (a -> a -> a) -> a -> a -> Bool
+commutative f x y = f x y == f y x
+
 main :: IO ()
 main =
   hspec $
-  describe "Formal power series" $ do
-    modifyMaxSuccess (const 10) $
-      it "monoProd has a neutral element" $
+  describe "Formal power series" $
+    -- modifyMaxSuccess (const 500) $
+   do
+    it "monoProd has a neutral element" $
       property (\a -> monoProd (a :: Mono) (Mono 1 0) == a)
+    it "monoProd is associative" $ property (associative monoProd)
+    it "mSum is associative" $
+      property (\a b c -> pSuml [a, b] [c] == pSumr [a, b] [c])
+    it "monoProd is commutative" $ property (commutative monoProd)
